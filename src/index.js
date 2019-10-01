a3a470d62d06454b7277d82eddb70beaca0e2876
anilpdv <32708402+anilpdv@users.noreply.github.com>
2019-10-01 05:43:25 -0700

    udpating index.js
    adding error boundary for errors testing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import ErrorBoundary from './ErrorBoundary.jsx';
 import * as serviceWorker from './serviceWorker';
 import {
     BrowserRouter as Router
 } from 'react-router-dom'
 
-ReactDOM.render( < Router > < App / > < /Router> , document.getElementById('root'));
+ReactDOM.render( <ErrorBoundary> < Router > < App / > < /Router> </ErrorBoundary> , document.getElementById('root'));
         // If you want your app to work offline and load faster, you can change
         // unregister() to register() below. Note this comes with some pitfalls.
         // Learn more about service workers: https://bit.ly/CRA-PWA
